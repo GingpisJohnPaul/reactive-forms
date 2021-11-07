df9901c8bfc7e78caf389e4f1e735b04346400a9
GingpisJohnPaul <201910813@gordoncollege.edu.ph>
2021-11-07 10:26:03 +0800

    initial commit

---
+++ reciclica-app-main/src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebaseConfig: {
+    apiKey: "AIzaSyBoyIvrsBmwcc95b38x0gKqyrBtjpoqD0Y",
+    authDomain: "reciclica-200dd.firebaseapp.com",
+    projectId: "reciclica-200dd",
+    storageBucket: "reciclica-200dd.appspot.com",
+    messagingSenderId: "808789969606",
+    appId: "1:808789969606:web:12cc86f54ac665141225c7"
+  },
+  production: false
+};
